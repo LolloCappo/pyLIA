a3556a08785086b857288ac1d2fbc85648f8e76e
LolloCappo <lorenzocapponi@outlook.it>
2019-11-11 13:22:45 +0100

    comma

--- pyLIA.py
+++ pyLIA.py
@@ -14,7 +14,7 @@ def LIA(data,sampling_freq,load_freq):
         mag -- magnitude of locked-in signal [DL -- digital level]
         ph -- phase of locked-in signal [deg -- degrees]
     """
-    t = np.linspace(0,data.shape[0]/sampling_freq, data.shape[0]) # Time vector
+    t = np.linspace(0, data.shape[0]/sampling_freq, data.shape[0]) # Time vector
     sine = np.sin(load_freq*t*2*np.pi) # Sine wave at the expected thermal frequency (i.e. the load frequency)
     cosine = np.cos(load_freq*t*2*np.pi) # Cosine wave at the expected thermal frequency (i.e. the load frequency)
 
