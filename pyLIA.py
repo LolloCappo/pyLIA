86419e525d81f65c7405c0eb5a9000dfd26d41bd
LolloCappo <lorenzocapponi@outlook.it>
2020-01-14 16:37:03 +0100

    amplitude fixed

--- pyLIA.py
+++ pyLIA.py
@@ -14,7 +14,7 @@ def LIA(data,sampling_freq,load_freq):
         mag -- magnitude of locked-in signal [DL -- digital level]
         ph -- phase of locked-in signal [deg -- degrees]
     """
-    t = np.linspace(0, data.shape[0]/sampling_freq, data.shape[0]) # Time vector
+    t = np.linspace(0, data.shape[0]/sampling_freq, data.shape[0], endpoint = True) # Time vector
     sine = np.sin(load_freq*t*2*np.pi) # Sine wave at the expected thermal frequency (i.e. the load frequency)
     cosine = np.cos(load_freq*t*2*np.pi) # Cosine wave at the expected thermal frequency (i.e. the load frequency)
 
@@ -23,8 +23,8 @@ def LIA(data,sampling_freq,load_freq):
 
     L1 = S*data # sine matrix * data matrix
     L2 = C*data # cosine matrix * data matrix
-    Re = np.sum(L1 ,0)/data.shape[0] # real part
-    Img = np.sum(L2, 0)/data.shape[0] #imaginary part
+    Re = 2 * np.trapz(L1)/data.shape[0] # real part
+    Img = 2 * np.trapz(L2)/data.shape[0] #imaginary part
     
     mag = np.sqrt(Re**2 + Img**2) # magnitude
     ph = np.arctan(Img/Re) * 180/np.pi #phase in degrees
