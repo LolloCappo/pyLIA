918f5551f9ab4a41fe7e8ba96a603b63f1c20a8c
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-06-17 14:17:21 +0200

    Update pyLIA.py

--- pyLIA.py
+++ pyLIA.py
@@ -1,22 +1,30 @@
 __version__ = '0.3'
 import numpy as np
 
-def LIA(data,sampling_freq,load_freq):
+def LIA(data,fs,fl):
     """
-    Function to obtain magnitude and phase of a thermal signal usign a lock-in analyzer
+    Function to obtain magnitude and phase of a thermal acquisition usign a lock-in analyzer
     
     Arguments:
-        data {np.array} -- Sequence of thermal images [frames, height, width]
-        sampling_freq {float} -- Sampling frequency of thermal video [Hz]
-        load_freq {float} -- Frequency of excitaiton load [Hz]
+    ---------
+        data : array
+                Sequence of thermal images [frames, height, width]
+
+        fs   : float
+                Sampling frequency of thermal video [Hz]
+
+        fl   : float
+                Frequency of excitaiton load [Hz]
     
     Returns:
-        mag -- Magnitude of locked-in signal [units of thermal video]
-        ph -- Phase of locked-in signal [deg -- degrees]
+    -------
+        magnitude : Magnitude of locked-in signal [units of thermal video]
+        phase  : Phase of locked-in signal [deg -- degrees]
+
     """
-    t = np.linspace(0, data.shape[0]/sampling_freq, data.shape[0], endpoint = True) # Time vector
-    sine = np.sin(load_freq * t * 2 * np.pi) # Sine wave at the expected thermal frequency (i.e. the load frequency)
-    cosine = np.cos(load_freq * t * 2 * np.pi) # Cosine wave at the expected thermal frequency (i.e. the load frequency)
+    t = np.linspace(0, data.shape[0]/fs, data.shape[0], endpoint = True) # Time vector
+    sine = np.sin(fl * t * 2 * np.pi) # Sine wave at the expected thermal frequency (i.e. the load frequency)
+    cosine = np.cos(fl * t * 2 * np.pi) # Cosine wave at the expected thermal frequency (i.e. the load frequency)
 
     S = (np.ones((data.shape[0], data.shape[1], data.shape[2])).T*sine).T 
     C = (np.ones((data.shape[0], data.shape[1], data.shape[2])).T*cosine).T 
@@ -26,7 +34,7 @@ def LIA(data,sampling_freq,load_freq):
     Re = 2 * np.trapz(L1, axis = 0)/data.shape[0] # real part
     Img = 2 * np.trapz(L2, axis = 0)/data.shape[0] #imaginary part
     
-    mag = np.sqrt(Re**2 + Img**2) # magnitude
-    ph = np.arctan(Img/Re) * 180/np.pi #phase in degrees
+    magnitude = np.sqrt(Re**2 + Img**2) # magnitude
+    phase = np.arctan(Img/Re) * 180/np.pi #phase in degrees
 
-    return mag, ph
+    return magnitude, phase
