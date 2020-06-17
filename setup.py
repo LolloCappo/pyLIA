5789691410de21b1d336bef69ee32ece73841862
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-06-17 09:29:17 +0200

    encoding

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 with open('readme.md', 'r') as f:
-    readme = f.read()
+    readme = f.read(encoding="utf8")
     
 def parse_requirements(filename):
     ''' Load requirements from a pip requirements file '''
