c89ee218cdd79b199ea7140242784c9c6ef70a50
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-06-19 09:40:36 +0200

    req

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
-from setuptools import setup, Extension
-from pyLIA import __version__
-
-with open('README.rst', 'r', encoding='utf8') as f:
+with open('README.rst', 'r') as f:
     readme = f.read()
     
-def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
-        lines = []
-        for line in fd:
-            line.strip()
-            if line and not line.startswith("#"):
-                lines.append(line)
-    return lines
-
-requirements = parse_requirements('requirements.txt')
-
+from setuptools import setup, Extension
+from pyLIA import __version__
 if __name__ == '__main__':
     setup(name='pyLIA',
         version=__version__,
@@ -25,5 +12,5 @@ if __name__ == '__main__':
         url='https://github.com/LolloCappo/pyLIA',
         py_modules=['pyLIA'],
         long_description=readme,
-        install_requires=requirements
+        install_requires = numpy
       )
